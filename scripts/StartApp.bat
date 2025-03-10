0c860bfcfb60e92bc52feaccff7e629f1eb87b74
navarro2812 <falconnavarro123@gmail.com>
2025-03-09 21:55:57 -0500

    Revert "Correccion ruta StartApp.bat"
    This reverts commit c0524ffed493f662704fe4ce73c7532fb56ced2b.

--- scripts/StartApp.bat
+++ scripts/StartApp.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Iniciando Calculadora App...
 REM Inicia la aplicación usando el comando start, que abre el ejecutable
-start "" "C:\Deploy\CalculadoraApp\ProyectoConsola.exe"
+start "" "C:\Deploy\CalculadoraApp\Calculadora.exe"
 exit /B 0
