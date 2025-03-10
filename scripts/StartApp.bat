c94630969fadd0b8fee17afe3a1e7b8a24d44c9c
navarro2812 <falconnavarro123@gmail.com>
2025-03-09 20:39:56 -0500

    Update StartApp.bat

---
+++ scripts/StartApp.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Iniciando Calculadora App...
+REM Inicia la aplicación usando el comando start, que abre el ejecutable
+start "" "C:\Deploy\CalculadoraApp\Calculadora.exe"
+exit /B 0
